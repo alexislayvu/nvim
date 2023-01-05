8294de37f691c1642b6df582e9a6651f99e7fe70
alexislayvu <alexislayvu@gmail.com>
2023-01-05 00:06:33 -0800

    added keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,4 +10,17 @@ lsp.set_preferences({
     }
 })
 
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+vim.keymap.set("n", "go", vim.lsp.buf.type_definition)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help)
+vim.keymap.set("n", "<F2>", vim.lsp.buf.rename)
+vim.keymap.set("n", "<F4>", vim.lsp.buf.code_action)
+vim.keymap.set("n", "gl", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+
 lsp.setup()
