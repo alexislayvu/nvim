970984f9e63da6510bd8b99433a349668ad79228
alexislayvu <alexislayvu@gmail.com>
2023-01-04 22:42:41 -0800

    fixed issues with color of the indent lines

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,16 +1,19 @@
 local dracula = require("dracula")
 dracula.setup({
+    vim.cmd [[colorscheme dracula]],
+    vim.cmd [[highlight SignColumn guibg=none]],
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" }),
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }),
     italic_comment = true,
 })
 
-function ColorMyPencils(color)
-    color = color or "dracula"
-    vim.cmd.colorscheme(color)
-	
-    vim.cmd [[highlight SignColumn guibg=none]]
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+require('indent_blankline').setup {
+    show_current_context = true,
+    show_trailing_blankline_indent = false,
+    char_highlight_list = {
+        "IndentBlanklineIndent",
+    }
+} 
 
-end
-
-ColorMyPencils()
+vim.cmd [[highlight IndentBlanklineIndent guifg=#999999 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineContextChar guifg=#E5E5E5 gui=nocombine]]
