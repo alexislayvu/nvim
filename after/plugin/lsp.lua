48b63ecb5a361c749126f6c722da59e3d733bb6b
alexislayvu <alexislayvu@gmail.com>
2023-01-04 22:01:32 -0800

    remove mappings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,18 +1,6 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-})
-
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
-})
-
 lsp.set_preferences({
     sign_icons = {
         error = 'E',
