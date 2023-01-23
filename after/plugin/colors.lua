0e7faffdca6d473a198d7caf54cad7e0c407bdf9
alexislayvu <alexislayvu@gmail.com>
2023-01-22 18:58:12 -0800

    setup barbecue and bufferline plugins

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -12,8 +12,24 @@ require('indent_blankline').setup {
     show_trailing_blankline_indent = false,
     char_highlight_list = {
         "IndentBlanklineIndent",
-    }
-} 
+    },
+}
 
 vim.cmd [[highlight IndentBlanklineIndent guifg=#999999 gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineContextChar guifg=#E5E5E5 gui=nocombine]]
+
+require("barbecue").setup({
+    theme = {
+        normal = { bg = none },
+    },
+})
+
+require('bufferline').setup{}
+
+vim.cmd [[highlight BufferLineFill guibg=none]]
+vim.cmd [[highlight BufferLineBackground guifg=#7a7c9e, guibg=none]]
+vim.cmd [[highlight BufferLineBufferSelected guifg=white guibg=none]]
+
+--vim.cmd("hi IlluminatedWordText guibg=#239e79")
+vim.cmd("hi IlluminatedWordRead guibg=#525252")
+--vim.cmd("hi IlluminatedWordWrite guibg=#239e79")
